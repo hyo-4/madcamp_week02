6cd141c8870ac3b343204aed5d90e8667ceebe06
Seunghyo <shyo0218@naver.com>
2024-01-12 19:50:02 +0900

    feat: my location get & env 추가

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:client/pages/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:kakao_map_plugin/kakao_map_plugin.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-const String kakaoMapKey = "6ae35f23355a17bec9412b2288ef7d16";
-
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
+  String kakaoMapKey = dotenv.get('KAKAOMAP_API_KEY');
   WidgetsFlutterBinding.ensureInitialized();
   AuthRepository.initialize(appKey: kakaoMapKey);
   runApp(const MyApp());
--- client/lib/pages/map_page.dart
+++ client/lib/pages/map_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:kakao_map_plugin/kakao_map_plugin.dart';
-
-//String kakaoMapKey = "6ae35f23355a17bec9412b2288ef7d16";
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -10,7 +9,22 @@ class MapPage extends StatefulWidget {
   State<MapPage> createState() => _MapPageState();
 }
 
+Future<Position> getCurrentLocation() async {
+  LocationPermission permission = await Geolocator.requestPermission();
+  Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high);
+  print(position.latitude);
+  print(position.longitude);
+  return position;
+}
+
 class _MapPageState extends State<MapPage> {
+  @override
+  void initState() {
+    super.initState();
+    getCurrentLocation();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(

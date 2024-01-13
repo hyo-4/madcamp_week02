dd249bc22adbf26e6c0f28b401ccaec11eae783e
Seunghyo <shyo0218@naver.com>
2024-01-13 14:05:21 +0900

    feat: login to mainpage

--- client/lib/pages/map_page.dart
+++ client/lib/pages/map_page.dart
@@ -55,19 +55,58 @@ class _MapPageState extends State<MapPage> {
 
   @override
   Widget build(BuildContext context) {
-    LatLng currentLatLng = LatLng(currentPosition?.latitude ?? 37.7749,
-        currentPosition?.longitude ?? -122.4194);
-    print(currentLatLng.toString());
+    LatLng currentLatLng = LatLng(
+      currentPosition?.latitude ?? 37.7749,
+      currentPosition?.longitude ?? -122.4194,
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Map'),
       ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: currentLatLng,
-          zoom: 11.0,
-        ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: currentLatLng,
+              zoom: 11.0,
+            ),
+          ),
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(50.0),
+              topRight: Radius.circular(50.0),
+            ),
+            child: DraggableScrollableSheet(
+              initialChildSize: 0.3,
+              minChildSize: 0.3,
+              maxChildSize: 1.0,
+              builder:
+                  (BuildContext context, ScrollController scrollController) {
+                return ClipRRect(
+                  // Wrap DraggableScrollableSheet with ClipRRect
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(50.0),
+                    topRight: Radius.circular(50.0),
+                  ),
+                  child: Container(
+                    color: Colors.white,
+                    child: ListView.builder(
+                      controller: scrollController,
+                      itemCount: 8,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                          title: Text('Item $index'),
+                        );
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
